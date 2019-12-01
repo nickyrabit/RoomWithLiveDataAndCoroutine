2814e571a7790ded63abcf9fe44ce98d938473ab
Nicky Ngailo <nickyrabit@gmail.com>
2019-12-02 00:59:02 +0300

    wrtting comments to explain Dao and database

--- app/src/main/java/com/nickyrabit/roomwithlivedatakotlin/dao_interfaces/PetrolPriceDao.kt
+++ app/src/main/java/com/nickyrabit/roomwithlivedatakotlin/dao_interfaces/PetrolPriceDao.kt
@@ -6,12 +6,33 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.nickyrabit.roomwithlivedatakotlin.model.PetrolPrice
-
+/**
+ *Now there is something called Data Access Object or DAO.
+ *  They are the main component in your Room Database.
+ *  They process the queries and stuff
+ *
+ *
+ * You declare them by using interfaces.
+ * Now I am making a DAO called PetrolPriceDao.java
+ * that will perform CRUD operations (Create, Read, Update, Delete)
+ */
 @Dao
 interface PetrolPriceDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg c: PetrolPrice)
 
+    /*
+    * I have set a LIVEDATA on
+    *  PetrolPriceList which has the task of alerting all Observer object
+    * when there is a change in a particular data in our case,
+    * PetrolPrice.
+    *
+    *
+    * A quick explanation on code below,
+    *  @Query() is an annotation that will hold all SQLite queries
+    *  because Room can perform only some few basic queries
+    * without typing them.
+    * */
 
     @Query("SELECT * FROM 'PetrolPrice'")
     @OnConflictStrategy
--- app/src/main/java/com/nickyrabit/roomwithlivedatakotlin/db/FuelPriceDatabase.kt
+++ app/src/main/java/com/nickyrabit/roomwithlivedatakotlin/db/FuelPriceDatabase.kt
@@ -1,4 +1,7 @@
 package com.nickyrabit.roomwithlivedatakotlin.db
+/**Created By Nickyrabit
+ * nickyrabit@gmail.com
+ * nicky@nickylegnard.com*/
 
 import android.content.Context
 import androidx.room.Database
@@ -6,10 +9,25 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.nickyrabit.roomwithlivedatakotlin.dao_interfaces.PetrolPriceDao
 import com.nickyrabit.roomwithlivedatakotlin.model.PetrolPrice
+/**
+ *Look at the @Database annotation, we have defined
+ * the table we made earlier by calling it entities
+ * {PetrolPrice.class} you can name as any tables as you like there
+ *
+ *
+ * VERSION is the version of the table if you alter it you need to add a new version.
+ *
+ *exportSchema is used when you want to keep track of the  database versions. Turn it off in Production yoooooooo!!!
+ *
+ *
+ *
+ */
 
 @Database(entities = [PetrolPrice::class],version = 1)
 abstract class FuelPriceDatabase : RoomDatabase(){
 abstract fun petrolPriceDao():PetrolPriceDao;
+//Inside the code, we have this line inside a constructor which defines the database "Price Database" by using DatabaseBuilder method
+    //DatabaseBuilder Creates a RoomDatabase.Builder for a persistent database. Once a database is built, you should keep a reference to it and re-use it.
 
     companion object {
         @Volatile private var instance: FuelPriceDatabase? = null
