0a099686db9d7a207df268e7e590681f8145a04d
Nicky Ngailo <nickyrabit@gmail.com>
2019-12-02 01:00:32 +0300

    entoty are tables

--- app/src/main/java/com/nickyrabit/roomwithlivedatakotlin/model/PetrolPrice.kt
+++ app/src/main/java/com/nickyrabit/roomwithlivedatakotlin/model/PetrolPrice.kt
@@ -5,8 +5,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
+/**Created By Nickyrabit
+ * nickyrabit@gmail.com
+ * nicky@nickylegnard.com*/
 @Entity
 data class PetrolPrice (
+    //Entities are tables and columns
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "fuel_price") val fuel_price:Double
 
